19dc34a5b53f8c117a3af3f2572bf0247d8225a2
andrewvitrenko <avitrenko2004@gmail.com>
2022-10-29 23:33:06 +0200

    feat(home): cerated home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,29 @@
 import React, { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button } from '../../components/Button';
+import { ROUTES } from '../../constants';
+
+import congratulations from '../../assets/images/congratulations.png';
+import './styles/index.scss';
 
 export const Home: FC = () => {
-  return <div>home</div>;
+  const navigate = useNavigate();
+
+  const handleStartPress = () => {
+    navigate(ROUTES.GAME);
+  };
+
+  return (
+    <div className="home-wrapper">
+      <div className="home-container">
+        <div className="home-image-container">
+          <img src={congratulations} alt="greeting hand" />
+        </div>
+        <div className="home-content">
+          <h1 className="home-title">Who wants to be a millionaire?</h1>
+          <Button text="Start" onClick={handleStartPress} />
+        </div>
+      </div>
+    </div>
+  );
 };
