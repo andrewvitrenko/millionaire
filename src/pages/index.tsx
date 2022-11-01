5f8d90abd7d9694920db84e0ca5df37183e1d4e0
andrewvitrenko <avitrenko2004@gmail.com>
2022-11-01 12:42:13 +0100

    fix(router): changed to hashrouter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,16 @@
 import React, { FC } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import { ROUTES } from '../constants';
 import { Home } from './Home';
 import { Game } from './Game';
 import { GameOver } from './GameOver';
 
 export const Router: FC = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path={ROUTES.HOME} element={<Home />} />
       <Route path={ROUTES.GAME} element={<Game />} />
       <Route path={ROUTES.GAME_OVER} element={<GameOver />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
